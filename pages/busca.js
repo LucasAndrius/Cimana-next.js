caaf04ed6b7134ad98d3af73543adbedf1968edb
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-08 13:14:37 -0300

    definindo rota dinâmica e area movie

--- pages/busca.js
+++ pages/busca.js
@@ -1,12 +1,26 @@
 import Head from 'next/head';
-import Link from 'next/link';
+import {useState} from 'react';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home({list}) {
+
+  const [searchText, setSeartchText] = useState('');
+  const [movieList, setMovieList] = useState([]);
+
+  const handleSearch = async () =>{
+    if(searchText !== ''){
+      const result = await fetch(`http://localhost:3000/api/search?q=${searchText}`);
+      const json = await result.json();
+      
+      setMovieList(json.list);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Filmes hoje</title>
+        <title>Filmes hoje | Busca</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,16 +28,20 @@ export default function Home({list}) {
         <h1 className={styles.title}>
           Busca
         </h1>
-        <Link href="/busca">Ir para a Busca</Link>
+        <input type="text" value={searchText} onChange={e=>setSeartchText(e.target.value)}/>
+        Termo de busca: {searchText}
+        <button onClick={handleSearch}>Buscar</button>
+        <hr/>
+
         <ul>
-          {list.map(item=>(
-            <li>
-              <a href={`/movie/${item.id}`}>
-                  <img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} width="150"/><br/>
-                  {item.title}
-              </a>
-            </li>
-          ))}
+            {movieList.map(item=> (
+                <li>
+                    <a href={`/movie/${item.id}`}>
+                        <img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} width="150"/><br/>
+                        {item.title}
+                    </a>
+              </li>
+            ))}
         </ul>
       
       </main>
---
+++ pages/movie/[id].js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css';
+
+export default function MovieItem({info}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Filmes hoje</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+            {info.title}
+        </h1>
+        <Link href="/busca">Ir para a Busca</Link>
+        <p>{info.overview}</p>
+
+        <p>Nota {info.vote_average}</p>
+        <img src={`https://image.tmdb.org/t/p/original${info.backdrop_path}`} width="400"/>
+        <p>{info.release_date}</p>
+        
+      
+      </main>
+    </div>
+  )
+}
+
+//context recebe o valor da pasta [id]
+export async function getServerSideProps(context) {
+  const res = await fetch(`http://localhost:3000/api/movie/${context.params.id}`);
+  const json = await res.json();
+
+  console.log("JSON", json);
+
+  return{
+    props:{
+      info: json.info
+    }
+  };
+}
